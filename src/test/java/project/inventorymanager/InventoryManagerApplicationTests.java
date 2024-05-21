eb90e040b2e65d07e15d56c47854e08c2a941918
igortrohimcuk <hameleon009@gmail.com>
2024-05-21 12:02:07 +0300

    Init commit

---
+++ src/test/java/project/inventorymanager/InventoryManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package project.inventorymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
