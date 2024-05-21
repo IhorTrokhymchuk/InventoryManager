5c594990c71b477bab3f881a55e65fef9e13eaf3
igortrohimcuk <hameleon009@gmail.com>
2024-05-21 12:46:31 +0300

    Create Inventory entity

---
+++ src/main/java/project/inventorymanager/model/inventory/Inventory.java
@@ -0,0 +1,41 @@
+package project.inventorymanager.model.inventory;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+import project.inventorymanager.model.product.Product;
+import project.inventorymanager.model.warehouse.Warehouse;
+
+@Entity
+@Getter
+@Setter
+@SQLDelete(sql = "UPDATE inventories SET is_deleted = TRUE WHERE id = ?")
+@SQLRestriction("is_deleted = FALSE")
+@Table(name = "inventories")
+public class Inventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "warehouse_id", nullable = false)
+    private Warehouse warehouse;
+    @Min(value = 0)
+    @Column(nullable = false)
+    private Long quantity;
+    @Column(name = "is_deleted",nullable = false)
+    private boolean isDeleted = false;
+}
