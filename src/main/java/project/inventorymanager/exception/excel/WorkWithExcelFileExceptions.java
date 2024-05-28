271183af3fc56f7f1763d0e11bfb4241f52b92dc
igortrohimcuk <hameleon009@gmail.com>
2024-05-28 11:57:09 +0300

    Add task for delete statistic file

--- src/main/java/project/inventorymanager/exception/excel/WorkWithExcelFileExceptions.java
+++
@@ -1,7 +0,0 @@
-package project.inventorymanager.exception.excel;
-
-public class WorkWithExcelFileExceptions extends RuntimeException {
-    public WorkWithExcelFileExceptions(String message) {
-        super(message);
-    }
-}
---
+++ src/main/java/project/inventorymanager/exception/file/WorkWithFileExceptions.java
@@ -0,0 +1,7 @@
+package project.inventorymanager.exception.file;
+
+public class WorkWithFileExceptions extends RuntimeException {
+    public WorkWithFileExceptions(String message) {
+        super(message);
+    }
+}
